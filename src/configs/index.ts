c70eb05981a47d707f66cbe002edd6065b02137e
Edmorton1 <ghettoboy2106@gmail.com>
2025-11-23 09:50:27 +0500

    add indexes

---
+++ src/configs/index.ts
@@ -0,0 +1,4 @@
+import eslintBackendConfig from './eslint.backend.config.js';
+import eslintFrontendConfig from './eslint.frontend.config.js';
+
+export {eslintBackendConfig, eslintFrontendConfig};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-import eslintBackendConfig from './configs/eslint.backend.config.js';
-import eslintFrontendConfig from './configs/eslint.frontend.config.js';
-
-export {eslintBackendConfig, eslintFrontendConfig};
+export * from './configs';
