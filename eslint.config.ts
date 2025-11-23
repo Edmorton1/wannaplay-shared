8c8bb2c70a0e49f53941ec8d48c14dda35e405e3
Edmorton1 <ghettoboy2106@gmail.com>
2025-11-23 11:47:33 +0500

    Migrate from Biome

--- eslint.config.ts
+++
@@ -1,4 +0,0 @@
-import {defineConfig} from 'eslint/config';
-import {baseEslintConfig} from './src/configs/base.eslint.config.js';
-
-export default defineConfig({...baseEslintConfig});
--- src/configs/index.ts
+++
@@ -1,4 +0,0 @@
-import eslintBackendConfig from './eslint.backend.config.js';
-import eslintFrontendConfig from './eslint.frontend.config.js';
-
-export {eslintBackendConfig, eslintFrontendConfig};
